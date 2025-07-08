ef2df67fed344551605b344a9e907cc96d8104fe
Mian Junaid <mianjunaid2312@gmail.com>
2025-07-08 21:25:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,11 +1,23 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, ServerApiVersion } from 'mongodb'
 
-if (!process.env.MONGODB_URI) {
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 }
 
-const uri = process.env.MONGODB_URI
-const options = {}
+if (MONGODB_URI.includes('<user>') || MONGODB_URI.includes('<password>') || MONGODB_URI.includes('<cluster-url>')) {
+    throw new Error('Please update the MONGODB_URI in your .env file with your actual connection string from MongoDB Atlas.')
+}
+
+
+const options = {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+};
 
 let client: MongoClient
 let clientPromise: Promise<MongoClient>
@@ -18,13 +30,13 @@ if (process.env.NODE_ENV === 'development') {
   }
 
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options)
+    client = new MongoClient(MONGODB_URI, options)
     globalWithMongo._mongoClientPromise = client.connect()
   }
   clientPromise = globalWithMongo._mongoClientPromise
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options)
+  client = new MongoClient(MONGODB_URI, options)
   clientPromise = client.connect()
 }
 
