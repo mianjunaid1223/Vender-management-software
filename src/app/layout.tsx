e4ae64b5bc9b42c69b9d0edb1a01d2d7a5be478d
Mian Junaid <mianjunaid2312@gmail.com>
2025-07-08 20:47:20 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
         className={cn(
           "min-h-screen bg-background font-body antialiased"
         )}
+        suppressHydrationWarning={true}
       >
         <ThemeProvider
           attribute="class"
